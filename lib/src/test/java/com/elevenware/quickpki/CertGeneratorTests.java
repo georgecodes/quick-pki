bb0ac70933519cc78b3970b0ac7fa83c022a23f0
George McIntosh <george@georgemcintosh.com>
2023-10-29 09:38:25 +0000

    Issuing certs

--- lib/src/test/java/com/elevenware/quickpki/CertGeneratorTests.java
+++ lib/src/test/java/com/elevenware/quickpki/CertGeneratorTests.java
@@ -1,9 +1,12 @@
 package com.elevenware.quickpki;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openssl.PEMWriter;
 import org.junit.jupiter.api.Test;
 
 import javax.security.auth.x500.X500Principal;
+import java.io.IOException;
+import java.io.StringWriter;
 import java.security.PublicKey;
 import java.security.SignatureException;
 import java.security.cert.X509Certificate;
@@ -28,14 +31,17 @@ public class CertGeneratorTests {
                 .commonName("My Cert")
                 .startDate(startTime)
                 .endDate(endTime)
+                .isCa(true)
                 .build();
 
-        CertificateBundle bundle = generator.generate(info);
+        CertificateBundle bundle = generator.issue(info);
         X509Certificate certificate  = bundle.getCertificate();;
 
+        assertEquals("CN=My Cert", certificate.getSubjectDN().getName());
         assertEquals("CN=My Cert", certificate.getIssuerDN().getName());
         assertThat(startTime).isEqualToIgnoringNanos(dateToLocalDateTime(certificate.getNotBefore()));
         assertThat(endTime).isEqualToIgnoringNanos(dateToLocalDateTime(certificate.getNotAfter()));
+
         PublicKey key = certificate.getPublicKey();
         try {
             certificate.verify(key);
@@ -53,24 +59,55 @@ public class CertGeneratorTests {
         LocalDateTime endTime = startTime.plusMinutes(1L);
 
         CertInfo info = CertInfo.builder()
-                .commonName("My Cert")
+                .commonName("My Root Cert")
                 .startDate(startTime)
                 .endDate(endTime)
                 .build();
 
-        CertificateBundle bundle = generator.generate(info);
-        X509Certificate certificate  = bundle.getCertificate();;
+        CertificateBundle rootBundle = generator.issue(info);
+        X509Certificate rootCert  = rootBundle.getCertificate();
 
-        assertEquals("CN=My Cert", certificate.getIssuerDN().getName());
+        info = CertInfo.builder()
+                .commonName("My Cert")
+                .startDate(startTime)
+                .endDate(endTime)
+                .issuer(rootBundle)
+                .build();
+
+        CertificateBundle bundle = generator.issue(info);
+        X509Certificate certificate = bundle.getCertificate();
+
+        assertEquals("CN=My Cert", certificate.getSubjectDN().getName());
+        assertEquals("CN=My Root Cert", certificate.getIssuerDN().getName());
         assertThat(startTime).isEqualToIgnoringNanos(dateToLocalDateTime(certificate.getNotBefore()));
         assertThat(endTime).isEqualToIgnoringNanos(dateToLocalDateTime(certificate.getNotAfter()));
+
         PublicKey key = certificate.getPublicKey();
         try {
             certificate.verify(key);
+            fail("Self-signed");
         } catch (SignatureException signatureException) {
-            fail("Not self-signed");
         }
 
+        key = rootCert.getPublicKey();
+        try {
+            certificate.verify(key);
+        } catch (SignatureException signatureException) {
+            fail("Not issued by issuer");
+        }
+
+        X500Principal issuerX500Principal = certificate.getIssuerX500Principal();
+
+        dumpCert(certificate);
+
+    }
+
+    private void dumpCert(X509Certificate certificate) throws IOException {
+        StringWriter sw = new StringWriter();
+        try (PEMWriter pw = new PEMWriter(sw)) {
+            pw.writeObject(certificate);
+        }
+        System.out.println(sw.toString());
     }
 
     public LocalDateTime dateToLocalDateTime(Date dateToConvert) {
