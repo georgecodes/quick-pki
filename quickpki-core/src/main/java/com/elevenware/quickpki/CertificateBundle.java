1a878b8d2e479be065da2769ebec76500162996e
George McIntosh <george@georgemcintosh.com>
2024-09-23 09:42:15 +0100

    Added some javadoc. Delomboking something

--- quickpki-core/src/main/java/com/elevenware/quickpki/CertificateBundle.java
+++ quickpki-core/src/main/java/com/elevenware/quickpki/CertificateBundle.java
@@ -13,14 +13,25 @@ import java.security.cert.CertificateEncodingException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 
+/**
+ * @CertificateBundle
+ *
+ * This class packages a certificate with its keypair and issuer, if any.
+ * It adds convenience methods for checking if a certificate was issued by another certificate.
+ */
 public class CertificateBundle {
-
 
     private CertificateBundle issuer;
     private final X509Certificate certificate;
     private final JcaX509CertificateHolder holder;
     private final KeyPair keyPair;
 
+    /**
+     * Constructor for a CertificateBundle
+     * @param issuer The issuer of the certificate
+     * @param certificate The certificate
+     * @param keyPair The keypair associated with the certificate
+     */
     public CertificateBundle(CertificateBundle issuer, X509Certificate certificate, KeyPair keyPair) {
         this.issuer = issuer;
         this.certificate = certificate;
@@ -35,7 +46,11 @@ public class CertificateBundle {
         }
     }
 
-
+    /**
+     * Checks if the certificate was issued by the provided issuer
+     * @param issuer
+     * @return
+     */
     public boolean issuedBy(CertificateBundle issuer) {
         X509Certificate issuerCert = issuer.getCertificate();
         try {
@@ -54,14 +69,28 @@ public class CertificateBundle {
         return true;
     }
 
+    /**
+     * Returns the issuer of the certificate
+     * @return
+     */
+    public CertificateBundle getIssuer() {
+        return issuer;
+    }
+
+    /**
+     * Returns the x.509 certificate
+     * @return
+     */
     public X509Certificate getCertificate() {
         return certificate;
     }
+
 
     public String getCommonName() {
         X500Name x500Name = holder.getSubject();
         return x500Name.getRDNs(BCStyle.CN)[0].getFirst().getValue().toString();
     }
+
 
     public KeyPair getKeyPair() {
         return keyPair;
